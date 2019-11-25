21f32b9502bdd3e644d3656c1f41f4f8454eb2d2
rotil <42150485+rotil@users.noreply.github.com>
2019-11-25 12:15:06 +0330

    Create co2.js

---
+++ co2.js
@@ -0,0 +1,16 @@
+
+ Java.perform(function(){
+     console.log("\nco detection bypass with Frida");
+
+var attack = Java.use("coc.on.cocon");
+attack.onCreate.overload("android.os.Bundle").implementation = function(key_val) {
+	console.log(" key_val befor onCreate load: "+this.key_val.value);
+	  this.key_val.value=1;
+	  this.onCreate.overload("android.os.Bundle").call(this,key_val);
+	  console.log("key_val after onCreate load: "+this.key_val.value);
+	  console.log("Found instance: "+this.key_val.value);
+	};
+
+
+
+});
